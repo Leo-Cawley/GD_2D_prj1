a75a15cc834e531e3d23bc52c742489c9505fe47
Leo-Cawley <23632687@stu.mmu.ac.uk>
2025-03-01 16:10:07 +0000

    Barrel

---
+++ Source/GD_2D_prj1/Public/BreakableScenery.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "PaperSpriteComponent.h"
+#include "BreakableScenery.generated.h"
+
+UCLASS()
+class GD_2D_PRJ1_API ABreakableScenery : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABreakableScenery();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visuals")
+	UPaperSpriteComponent* Sprite;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	UBoxComponent* Collider;
+	
+
+public:	
+	// Called every frame
+	/*virtual void Tick(float DeltaTime) override;*/
+	
+
+
+
+};
